87ac8a85a2b4ec6fd1f4c176673e9fd85ac80bb2
Imad Kariapper <imad.kariapper@yodle.com>
2015-10-18 23:08:56 -0400

    War game kata

--- card-game-war/src/card_game_war/game.clj
+++ card-game-war/src/card_game_war/game.clj
@@ -1,13 +1,28 @@
 (ns card-game-war.game)
 
-;; feel free to use these cards or use your own data structure
-(def suits [:spade :club :diamond :heart])
-(def ranks [2 3 4 5 6 7 8 9 10 :jack :queen :king :ace])
-(def cards
-  (for [suit suits
-        rank ranks]
-    [suit rank]))
+;; cards represented by 0-51. 0-12=2,3,4...,J,Q,K,A
+;; spades = 0-12, clubs = 13-25, diamonds = 26-38, hearts = 39-51
 
-(defn play-round [player1-card player2-card])
+;; TODO: take in map of player1->card to clean things up
+(defn compare-cards [player1-card player2-card]
+  (let [ranks (map #(-> (mod % 13)) [player1-card player2-card])
+        suits (map #(-> (quot % 13)) [player1-card player2-card])]
+    (cond
+      (> (nth ranks 0) (nth ranks 1)) "player1"
+      (< (nth ranks 0) (nth ranks 1)) "player2"
+      (> (nth suits 0) (nth suits 1)) "player1"
+      (< (nth suits 0) (nth suits 1)) "player2")))
 
-(defn play-game [player1-cards player2-cards])
+(defn play-round [player1-cards player2-cards]
+  (let [[player1-card & player1-deck] player1-cards
+        [player2-card & player2-deck] player2-cards
+        winner (compare-cards player1-card player2-card)]
+    (cond
+      (= winner "player1") [(conj player1-deck player1-card player2-card) player2-deck]
+      (= winner "player2") [player1-deck (conj player2-deck player2-card player1-card)])))
+
+(defn play-game [player1-cards player2-cards]
+  (cond
+    (empty? player1-cards) "player2"
+    (empty? player2-cards) "player1"
+    :else (apply play-game (play-round player1-cards player2-cards))))
--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -4,15 +4,25 @@
 
 
 ;; fill in  tests for your game
-(deftest test-play-round
+(deftest test-compare-cards
   (testing "the highest rank wins the cards in the round"
-    (is (= 0 1)))
-  (testing "queens are higher rank than jacks")
-  (testing "kings are higher rank than queens")
-  (testing "aces are higher rank than kings")
-  (testing "if the ranks are equal, clubs beat spades")
-  (testing "if the ranks are equal, diamonds beat clubs")
-  (testing "if the ranks are equal, hearts beat diamonds"))
+    (is (= "player1" (compare-cards 11 30))))
+  (testing "queens are higher rank than jacks"
+    (is (= "player2" (compare-cards 9 10))))
+  (testing "kings are higher rank than queens"
+    (is (= "player1" (compare-cards 11 10))))
+  (testing "aces are higher rank than kings"
+    (is (= "player2" (compare-cards 11 12))))
+  (testing "if the ranks are equal, clubs beat spades"
+    (is (= "player1" (compare-cards 13 0))))
+  (testing "if the ranks are equal, diamonds beat clubs"
+    (is (= "player2" (compare-cards 13 26))))
+  (testing "if the ranks are equal, hearts beat diamonds"
+    (is (= "player1" (compare-cards 39 26)))))
+
+(deftest test-play-round
+  (testing "player1 has empty deck after round"
+    (is (= [nil [7 3]] (play-round [3] [7])))))
 
 (deftest test-play-game
   (testing "the player loses when they run out of cards"))
